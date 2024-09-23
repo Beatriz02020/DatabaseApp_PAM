fa9da4aa088ae22c66d6efc6b4e5df54622b6307
Beatriz02020 <biatriz9andrade@gmail.com>
2024-09-23 00:32:08 -0300

    pronto

--- app/src/main/java/com/example/appdatabase/MainActivity.kt
+++ app/src/main/java/com/example/appdatabase/MainActivity.kt
@@ -5,44 +5,36 @@ package com.example.appdatabase
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.room.Room
+import com.example.appdatabase.roomDB.Pessoa
 import com.example.appdatabase.roomDB.PessoaDataBase
 import com.example.appdatabase.ui.theme.AppDatabaseTheme
 import com.example.appdatabase.viewModel.PessoaViewModel
@@ -50,20 +42,6 @@ import com.example.appdatabase.viewModel.Repository
 
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            AppDatabaseTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    App()
-                }
-            }
-        }
-    }
     private val db by lazy {
         Room.databaseBuilder(
             applicationContext,
@@ -71,6 +49,7 @@ class MainActivity : ComponentActivity() {
             "pessoa.db"
         ).build()
     }
+
     private val viewModel by viewModels<PessoaViewModel>(
         factoryProducer = {
             object : ViewModelProvider.Factory {
@@ -80,16 +59,36 @@ class MainActivity : ComponentActivity() {
             }
         }
     )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AppDatabaseTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    App(viewModel, this)
+                }
+            }
+        }
+    }
 }
 
 @Composable
-fun App() {
+fun App(viewModel: PessoaViewModel, mainActivity: MainActivity) {
     var nome by remember{
         mutableStateOf("")
     }
     var telefone by remember {
         mutableStateOf("")
     }
+    var pessoa = Pessoa(
+        nome,
+        telefone
+    )
     Column(
         Modifier
             .background(Color.White)
@@ -153,9 +152,11 @@ fun App() {
                 .fillMaxWidth(),
             Arrangement.Center
         ) {
-            Button(onClick = {  /*TODO*/  }
-
-            ) {
+            Button(onClick = {
+                viewModel.upsertPessoa(pessoa)
+                nome = ""
+                telefone = ""
+            }) {
                 Text( "Cadastrar")
             }
         }
@@ -168,15 +169,7 @@ fun App() {
     }
 }
 
-@Composable
-fun LabelAndPlaceHolder(Name: String) {
-    var text by remember { mutableStateOf(TextFieldValue("")) }
-    TextField(
-        value = text,
-        onValueChange = {
-            text = it
-        },
-        label = { Text(text = Name) },
-        placeholder = { Text(text = " ") },
-    )
-}
+
+
+
+
